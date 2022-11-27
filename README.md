4bd849bf668f8ab18688f8781a4418b9751d11d0
valeroman <valeraromanovo@gmail.com>
2022-11-28 01:14:36 +0300

    Set project settings for working with pug and sass

--- README.md
+++
@@ -1,2 +0,0 @@
-# nootris2
-GULP
---
+++ gulpfile.js
@@ -0,0 +1,90 @@
+let preprocessor = 'sass';
+
+const { src, dest, parallel, series, watch, gulp } = require('gulp');
+const size = require('gulp-size');
+const gulppug = require('gulp-pug');
+const browserSync = require('browser-sync').create();
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify-es').default;
+const sass = require('gulp-sass')(require('sass'));
+const autoprefixer = require('gulp-autoprefixer');
+const cleancss = require('gulp-clean-css');
+const imagecomp = require('compress-images');
+const clean = require('gulp-clean');
+
+
+function styles() {
+	return src('app/' + preprocessor + '/main.' + preprocessor + '') // Выбираем источник: "app/sass/main.sass" или "app/less/main.less"
+	.pipe(eval(preprocessor)()) // Преобразуем значение переменной "preprocessor" в функцию
+	.pipe(concat('app.min.css')) // Конкатенируем в файл app.min.js
+	.pipe(autoprefixer({ overrideBrowserslist: ['last 10 versions'], grid: true })) // Создадим префиксы с помощью Autoprefixer
+	.pipe(cleancss( { level: { 1: { specialComments: 0 } }/* , format: 'beautify' */ } )) // Минифицируем стили
+	.pipe(dest('app/css/')) // Выгрузим результат в папку "app/css/"
+	.pipe(browserSync.stream()) // Сделаем инъекцию в браузер
+}
+
+function browsersync() {
+	browserSync.init({ // Инициализация Browsersync
+		server: { baseDir: 'app/' }, // Указываем папку сервера
+		notify: false, // Отключаем уведомления
+		online: true // Режим работы: true или false
+	})
+}
+
+async function images() {
+	imagecomp(
+		"app/images/src/**/*", // Берём все изображения из папки источника
+		"app/images/dest/", // Выгружаем оптимизированные изображения в папку назначения
+		{ compress_force: false, statistic: true, autoupdate: true }, false, // Настраиваем основные параметры
+		{ jpg: { engine: "mozjpeg", command: ["-quality", "75"] } }, // Сжимаем и оптимизируем изображеня
+		{ png: { engine: "pngquant", command: ["--quality=75-100", "-o"] } },
+		{ svg: { engine: "svgo", command: "--multipass" } },
+		{ gif: { engine: "gifsicle", command: ["--colors", "64", "--use-col=web"] } },
+		function (err, completed) { // Обновляем страницу по завершению
+			if (completed === true) {
+				browserSync.reload()
+			}
+		}
+	)
+}
+
+function cleanimg() {
+	return src('app/images/dest/', {allowEmpty: true}).pipe(clean()) // Удаляем папку "app/images/dest/"
+}
+
+function cleandist() {
+	return src('dist', {allowEmpty: true}).pipe(clean()) // Удаляем папку "dist/"
+}
+
+function startwatch() {
+	watch('app/**/' + preprocessor + '/**/*', styles);
+    watch('app/**/*.згп', pug);
+    watch('app/images/src/**/*', images); 
+}
+
+function buildcopy() {
+	return src([ // Выбираем нужные файлы
+		'app/css/**/*.min.css',
+		'app/images/dest/**/*',
+		'app/**/*.pug',
+		], { base: 'app' }) // Параметр "base" сохраняет структуру проекта при копировании
+	.pipe(dest('dist')) // Выгружаем в папку с финальной сборкой
+}
+
+function pug() {
+    return src('app/*.pug')
+    .pipe(gulppug())
+    .pipe(size({
+        showFile: true
+    }))
+    .pipe(dest('dist/'))
+    .pipe(browserSync.stream())
+}
+
+exports.pug = pug;
+exports.browsersync = browsersync;
+exports.styles = styles;
+exports.images = images;
+exports.cleanimg = cleanimg;
+exports.build = series(pug, cleandist, styles, images, buildcopy);
+exports.default = parallel(pug, styles, browsersync, startwatch);
