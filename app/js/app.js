aaf9ad621c8d781f74dbbb67c38fb2de22f60489
valeroman <valeraromanovo@gmail.com>
2022-11-28 20:03:56 +0300

    Finish button, doesn't work

--- app/js/app.js
+++ app/js/app.js
@@ -1,9 +1,9 @@
 const menuButton = document.querySelector('.header__show-button');
-let menu = document.querySelector('.menu');
+const menu = document.querySelector('.menu');
 
 menuButton.addEventListener('click', (e) => {
-    e.preventDefault();
     handleMenu();
+    e.preventDefault();
 });
 function handleMenu() {
     const display = window.getComputedStyle(menu, null).getPropertyValue("display");
